4207827ae26c6a2d4135e4cb9067874efc3e3fc1
gitpro <vinuthasrinivas5@gmail.com>
2020-06-08 18:57:43 +0530

    unit tests added

--- tests/unit/ListPosts.spec.js
+++ tests/unit/ListPosts.spec.js
@@ -1,12 +1,20 @@
 import { shallowMount,mount,createLocalVue} from '@vue/test-utils'
 import ListPosts from '@/views/ListPosts.vue'
 
-const wrapper = shallowMount(ListPosts)
 
 describe('ListPosts', () => {
-
+  const wrapper = shallowMount(ListPosts)
   it('has a created hook',()=>{
     expect (typeof ListPosts.created).toBe('function')
+  })
+  it('has edit method',()=>{
+    
+
+    expect(typeof wrapper.vm.Edit).toBe('function')
+  })
+  it('has all required methods',()=>{
+    expect(typeof wrapper.vm.prevPage).toBe('function')
+    expect(typeof wrapper.vm.nextPage).toBe('function')
   })
 
 })
